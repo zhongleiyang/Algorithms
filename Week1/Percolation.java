78c7048a5f9cde4df612fd258017d846a852725a
zhonglei <zhleiyang@gmail.com>
2013-02-25 18:15:15 +0800

    Add the Percolation class

---
+++ Week1/Percolation.java
@@ -0,0 +1,27 @@
+public class Percolation 
+{
+   public Percolation(int N)              // create N-by-N grid, with all sites blocked
+   {
+       
+   }
+   public void open(int i, int j)         // open site (row i, column j) if it is not already
+   {
+       
+   }
+   public boolean isOpen(int i, int j)    // is site (row i, column j) open?
+   {
+       return true;
+   }
+   public boolean isFull(int i, int j)    // is site (row i, column j) full?
+   {
+       return true;
+   }
+   public boolean percolates()            // does the system percolate?
+   {
+       return true;
+   }
+    public static void main(String[] args)
+   {  
+
+   } 
+}
