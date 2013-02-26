0423cc61bb961751bbb5f8357dbfd0e5a22adf61
zhonglei <zhleiyang@gmail.com>
2013-02-26 13:33:51 +0800

    fix the exception

--- Week1/PercolationStats.java
+++ Week1/PercolationStats.java
@@ -5,6 +5,8 @@ public class PercolationStats
     
    public PercolationStats(int N, int T)    // perform T independent computational experiments on an N-by-N grid
    {
+       if(N <= 0 || T <= 0)
+           throw new IllegalArgumentException();
        times = T;
        percolationThresholds = new double[T];
        for(int time = 0; time < T; time++)
@@ -44,18 +46,11 @@ public class PercolationStats
    }
    public static void main(String[] args)   // test client, described below
    {
-       if (args.length == 2 && Integer.parseInt(args[0]) > 0 && Integer.parseInt(args[1]) > 0) 
-       {
-           int N = Integer.parseInt(args[0]);        
-           int T = Integer.parseInt(args[1]);    
-           PercolationStats percolationStats = new PercolationStats(N, T);
-           StdOut.println("mean" + "                    = " + percolationStats.mean());
-           StdOut.println("stddev" + "                  = " + percolationStats.stddev());
-           StdOut.println("95% confidence interval"  + " = " + percolationStats.confidenceLo() + ", " + percolationStats.confidenceHi());
-       }
-       else
-       {
-           throw new RuntimeException("Invalid arguments");
-       }
+       int N = Integer.parseInt(args[0]);        
+       int T = Integer.parseInt(args[1]);    
+       PercolationStats percolationStats = new PercolationStats(N, T);
+       StdOut.println("mean" + "                    = " + percolationStats.mean());
+       StdOut.println("stddev" + "                  = " + percolationStats.stddev());
+       StdOut.println("95% confidence interval"  + " = " + percolationStats.confidenceLo() + ", " + percolationStats.confidenceHi());
    }
 }
