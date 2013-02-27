572ace107caa703805b14f5f3054e19bec6503cc
zhonglei <zhleiyang@gmail.com>
2013-02-27 18:46:42 +0800

    add for backup 2

--- Week1/Percolation.java
+++ Week1/Percolation.java
@@ -7,14 +7,10 @@
     private boolean isFullChange;
     private boolean isPercolate;
     private int count;
-    private int[] lastRowOpenIds;
-    private int lastRowOpenIdsLength;
     
     public Percolation(int N)              // create N-by-N grid, with all sites blocked
-    {
+    {       
         width = N + 1;
-        lastRowOpenIds = new int[width];
-        lastRowOpenIdsLength = 0;
         status = new boolean[width * width];
         isFullArray = new boolean[width * width];
         for(int i= 0; i < width * width; i++)
@@ -35,7 +31,7 @@
             throw new IndexOutOfBoundsException ();
         
         if(!isOpen(i,j))
-        {       
+        {         
             count++;
             status[i * width + j] = true;
             
@@ -44,9 +40,9 @@
                 isFullArray[i * width + j] = true;
                 isFullChange = true;
             }
-                     
+            
             if(i > 1 && isOpen(i - 1, j) == true) 
-            {
+            {         
                 int parent1 = uf.find((i - 1) * width + j);
                 int parent2 = uf.find(i * width + j); 
                 if(parent1 != parent2) 
@@ -55,6 +51,7 @@
                     {
                         isFullArray[parent1] = true;
                         isFullArray[parent2] = true;
+                        isFullArray[i * width + j] = true;
                         isFullChange = true;
                     }
                     uf.union((i - 1) * width + j, i * width + j); 
@@ -71,6 +68,7 @@
                     {
                         isFullArray[parent1] = true;
                         isFullArray[parent2] = true;
+                        isFullArray[i * width + j] = true;
                         isFullChange = true;
                     }
                     uf.union(i * width + j - 1, i * width + j); 
@@ -87,6 +85,7 @@
                     {
                         isFullArray[parent1] = true;
                         isFullArray[parent2] = true;
+                        isFullArray[i * width + j] = true;
                         isFullChange = true;
                     }
                     uf.union((i + 1) * width + j, i * width + j);  
@@ -103,24 +102,12 @@
                     {
                         isFullArray[parent1] = true;
                         isFullArray[parent2] = true;
+                        isFullArray[i * width + j] = true;
                         isFullChange = true;
                     }
                     uf.union(i * width + j + 1, i * width + j); 
                 }
-            }
-            if(i == width -1)
-            {
-                int k;
-                for(k = 0; k < lastRowOpenIdsLength; k++)
-                    if(uf.connected((width - 1) * width + lastRowOpenIds[k], i * width + j))   
-                           break;
-                       
-                if(k == lastRowOpenIdsLength) 
-                {
-                    lastRowOpenIds[lastRowOpenIdsLength] = j;
-                    lastRowOpenIdsLength++;
-                }
-            }
+            }            
         }       
     }
     public boolean isOpen(int i, int j)    // is site (row i, column j) open?
@@ -133,24 +120,64 @@
     {
         if(i < 1 || i > width - 1 || j < 1 || j > width - 1)
             throw new IndexOutOfBoundsException ();
+        if(!isOpen(i, j))
+            return false;
+        
         if(isFullArray[i * width + j])
             return true;
+        
+        if(isIsland(i, j))
+            return false;
+        
         return isFullArray[uf.find(i * width + j)];      
     }
+    
+    private boolean isIsland(int i, int j)
+    {  
+        if(!isOpen(i, j))
+            return true;
+        
+        if(i > 1 && isOpen(i - 1, j))
+            return false;
+        
+        if(j > 1 && isOpen(i, j - 1))
+            return false;
+        
+        if(i < width - 1 && isOpen(i + 1, j))
+            return false;
+        
+        if(j < width - 1 && isOpen(i, j + 1))
+            return false;
+        
+        return true;
+    }
+    
     public boolean percolates()            // does the system percolate?
     {
         if(isPercolate)
             return true;
             
-        if(count >= width - 1 && isFullChange)
+        if(count >= width - 1&& isFullChange)
         {
             isFullChange = false;
-            for(int i = 0; i < lastRowOpenIdsLength; i++)
+            boolean perStatu = false;
+            for(int i = 1; i < width; i++)
             {
-                if(isFull(width - 1, lastRowOpenIds[i]))
-                { 
-                    isPercolate = true;
-                    return true;
+                if(isOpen(width - 1, i) != perStatu)
+                {
+                    if(isOpen(width - 1, i) == true)
+                    {
+                        perStatu = true;
+                        if(isFull(width - 1, i))
+                        {
+                            isPercolate = true;
+                            return true;
+                        }
+                    }
+                    else
+                    {
+                        perStatu  = false;
+                    }
                 }
             }
         }
